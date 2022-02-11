978585d66277b4fa3fd849f74748a17df3646415
DiegoElNegro <diego.g729@slcstudents.org>
2022-02-11 14:52:54 -0700

    I finished 🥵

---
+++ app.js
@@ -0,0 +1,22 @@
+// set inital count
+let count = 0;
+
+// selcect value and buttons
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
+
+btns.forEach(function (btn) {
+    btn.addEventListener("click", function (e) {
+        const styles = e.currentTarget.classList;
+        if(styles.contains("decrease")){
+            count--;
+        }
+        else if(styles.contains("increase")){
+        count++;
+        }
+        else{
+            count = 0;
+        }
+        value.textContent = count;
+    })
+});
